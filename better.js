97dadd133f1ab3505b4e0d03c3c668624faa109c
badger-tall <badger2tall@gmail.com>
2017-03-03 06:52:08 +0300

    Added a few test line, inckuding some \n and \n\r to test, ow it works from bash CLI ( like >>>echo "\n\r test line" >> better.js

--- better.js
+++ better.js
@@ -2,4 +2,6 @@ better is better
 resetter is here already... beware of him
 some new line #1
 some old line #2
-some new line #3
+some new line #3this is next page
+\n\nthis is next page
+\n\r\n\rthis is next page
